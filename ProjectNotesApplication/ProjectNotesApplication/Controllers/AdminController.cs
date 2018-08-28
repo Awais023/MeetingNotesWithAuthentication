e7b8b542bacb194621066af27bb3da9a17c77441
Awais023 <042waisi@gmail.com>
2018-08-28 14:27:08 +0500

    With Authentication

---
+++ ProjectNotesApplication/ProjectNotesApplication/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ProjectNotesApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectNotesApplication.Controllers
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class AdminController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(FormCollection form)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            string userName = form["txtEmail"];
+            string email = form["txtEmail"];
+            string pwd = form["txtpassword"];
+
+            var user = new ApplicationUser();
+            user.UserName = userName;
+            user.Email = email;
+            var newuser = userManager.Create(user, pwd);
+            return View();
+
+        }
+        public ActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewRole(FormCollection Form)
+        {
+            String rolename = Form["RoleName"];
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if(!roleManager.RoleExists(rolename))
+            {
+                //CREATE SUPER ADMIN ROLE
+                var role = new IdentityRole(rolename);
+                roleManager.Create(role);
+            }
+            return View("Index");
+        }
+
+    }
+}
---
+++ ProjectNotesApplication/ProjectNotesApplication/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using ProjectNotesApplication.Models;
+
+[assembly: OwinStartupAttribute(typeof(ProjectNotesApplication.Startup))]
+namespace ProjectNotesApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateUserAndRoles();
+        }
+        public void CreateUserAndRoles()
+        { 
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("SuperAdmin"))
+            {
+                var role = new IdentityRole("SuperAdmin");// CREATE SUPER ADMIN ROLE
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();//CREATE DEFAULT USER
+                user.UserName = "sa@domain.com";
+                user.Email = "sa@domain.com";
+                string pwd = "Password@2017";
+                var newuser = userManager.Create(user, pwd);
+                if (newuser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "SuperAdmin");
+                }
+            }
+        }
+    }
+}
+
